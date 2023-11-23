757168bc51f631a4ccb64b3df2fad53f01e31275
TerminalEagle <affan.shaikh2309@gmail.com>
2023-11-23 12:14:40 +0530

    added title, meta description and favicon

--- app/(planet)/[slug]/page.tsx
+++ app/(planet)/[slug]/page.tsx
@@ -4,6 +4,7 @@ import ContentTab from "@/components/ContentTab";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Statistics from "@/components/Statistics";
 import planets from "@/data";
+import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -39,27 +40,29 @@ const Planet = ({ params }: Props) => {
   }
 
   return (
-    <main>
-      <MaxWidthWrapper>
-        <section className="information">
-          <div className="planet-image">
-            <Image priority={true} src={image} alt={`illustration of ${planetInfo.slug}`} width={0} height={0} style={{ width: "70%", height: "100%" }} />
-            {contentTab === "geology" && (
-              <Image
-                src={`/geology-${planetInfo.slug}.png`}
-                alt={`geology of ${planetInfo.slug}`}
-                width={126}
-                height={126}
-                style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, 0%)" }}
-              />
-            )}
-          </div>
-          <Content name={planetInfo.name} data={planetInfo[contentTab]} />
-          <ContentTab contentTab={contentTab} setContentTab={setContentTab} />
-        </section>
-        <Statistics rotation={rotation} revolution={revolution} radius={radius} temperature={temperature} />
-      </MaxWidthWrapper>
-    </main>
+    <>
+      <main>
+        <MaxWidthWrapper>
+          <section className="information">
+            <div className="planet-image">
+              <Image priority={true} src={image} alt={`illustration of ${planetInfo.slug}`} width={0} height={0} style={{ width: "70%", height: "100%" }} />
+              {contentTab === "geology" && (
+                <Image
+                  src={`/geology-${planetInfo.slug}.png`}
+                  alt={`geology of ${planetInfo.slug}`}
+                  width={126}
+                  height={126}
+                  style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, 0%)" }}
+                />
+              )}
+            </div>
+            <Content name={planetInfo.name} data={planetInfo[contentTab]} />
+            <ContentTab contentTab={contentTab} setContentTab={setContentTab} />
+          </section>
+          <Statistics rotation={rotation} revolution={revolution} radius={radius} temperature={temperature} />
+        </MaxWidthWrapper>
+      </main>
+    </>
   );
 };
 
